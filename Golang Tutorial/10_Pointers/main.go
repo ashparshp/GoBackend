0f3b4ff65ab65d0831c29d64dfd3b113a9c31762
Ashparshp <ashparsh.connects@gmail.com>
2025-03-08 09:41:36 +0530

    Add examples of pass by value and pass by reference using pointers in main.go

--- Golang Tutorial/10_Pointers/main.go
+++ Golang Tutorial/10_Pointers/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+// by default, Go uses pass by value
+func changeNum(num int) {
+	num = 5
+	fmt.Println("num in changeNum:", num)
+}
+
+// pass by reference
+func changeNumByReference(num *int) {
+	*num = 5
+	fmt.Println("num in changeNumByReference:", *num)
+}
+
 func main() {
+	num := 1
+	
+	// pass by value
+	fmt.Println("num before changeNum:", num)
+	changeNum(num)
+	fmt.Println("num after changeNum:", num)
+
+	// to pass by reference, use pointers
+	fmt.Println("num before changeNumByReference:", num)
+	changeNumByReference(&num)
+	fmt.Println("num after changeNumByReference:", num)
 
 }
