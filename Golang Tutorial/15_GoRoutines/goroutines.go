11f667e9b65b37ba829a3799bd44f7705ee7176b
Ashparshp <ashparsh.connects@gmail.com>
2025-03-10 11:49:27 +0530

    Refactor goroutines example to use WaitGroup for synchronization

--- Golang Tutorial/15_GoRoutines/goroutines.go
+++ Golang Tutorial/15_GoRoutines/goroutines.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println("Task", id)
 }
 
 func main() {
+
+	var wg sync.WaitGroup
+
+
 	for i := 0; i < 10; i++ {
-		go task(i)
+		wg.Add(1)
+		go task(i, &wg)
 	}
 
-	time.Sleep(time.Second)
+	wg.Wait()
 	fmt.Println("Done")
 }
