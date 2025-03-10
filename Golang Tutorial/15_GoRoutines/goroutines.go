5042e9a68c42d962b83feb19627de9cc373c5032
Ashparshp <ashparsh.connects@gmail.com>
2025-03-10 11:43:52 +0530

    Add example of goroutines in goroutines.go

---
+++ Golang Tutorial/15_GoRoutines/goroutines.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int) {
+	fmt.Println("Task", id)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go task(i)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+}
