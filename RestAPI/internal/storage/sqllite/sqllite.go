25d599b2f7417c1b5851d331e1bc923473dbd113
Ashparshp <ashparsh.connects@gmail.com>
2025-03-16 16:11:26 +0530

    GetbyId

--- RestAPI/internal/storage/sqllite/sqllite.go
+++ RestAPI/internal/storage/sqllite/sqllite.go
@@ -2,8 +2,10 @@ package sqllite
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/ashparshp/students-api/internal/config"
+	"github.com/ashparshp/students-api/internal/types"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -52,4 +54,25 @@ func (s *Sqllite) CreateStudent(name string, email string, age int) (int64, erro
 	}
 
 	return lastId, nil
+}
+
+func (s *Sqllite) GetStudentById(id int64) (types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT * FROM students WHERE id = ? LIMIT 1")
+	if err != nil {
+		return types.Student{}, err
+	}
+
+	defer stmt.Close()
+
+	var student types.Student
+
+	err = stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+	if err != nil {
+        if err == sql.ErrNoRows {
+            return types.Student{}, fmt.Errorf("no student found with id %s", fmt.Sprint(id))
+        }
+        return types.Student{}, fmt.Errorf("query error: %w", err)
+    }
+
+	return student, nil
 }
