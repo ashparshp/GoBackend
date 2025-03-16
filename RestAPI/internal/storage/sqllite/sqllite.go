93ebd63160253fb7b4314896efa1ef99bd6b47b7
Ashparshp <ashparsh.connects@gmail.com>
2025-03-16 19:51:47 +0530

    Students list routes

--- RestAPI/internal/storage/sqllite/sqllite.go
+++ RestAPI/internal/storage/sqllite/sqllite.go
@@ -76,3 +76,35 @@ func (s *Sqllite) GetStudentById(id int64) (types.Student, error) {
 
 	return student, nil
 }
+
+func (s *Sqllite) GetStudents() ([]types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students")
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+    }
+
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+
+		err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+		if err != nil {
+			return nil, err
+		}
+
+		students = append(students, student)
+	}
+
+
+	return students, nil
+}
