701220dca18404896ecdcea6a04befeecc480108
Ashparshp <ashparsh.connects@gmail.com>
2025-03-31 08:43:34 +0530

    Better Caching... Dynamic File Access

--- WebGo/02_Serving_HTML/pkg/render/render.go
+++ WebGo/02_Serving_HTML/pkg/render/render.go
@@ -1,14 +1,16 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-// RenderTemplate renders a template
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+/*
+// RenderTemplate Simply renders a template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 
@@ -17,7 +19,10 @@ func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 		return
 	}
 }
+*/
 
+/*
+// RenderTemplate renders a template with cache
 var tc = make(map[string]*template.Template)
 func RenderTemplate(w http.ResponseWriter, t string) {
 	var tmpl *template.Template
@@ -59,4 +64,68 @@ func createTemplateCache(t string) error {
 	// add template to cache (map)
 	tc[t] = tmpl
 	return nil
+}
+*/
+
+// RenderTemplate renders a template
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// create a template cache
+	tc, err := createTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	// get all the files named *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
