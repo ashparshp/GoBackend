43c88f32c05d2569f1d52dacc73f8b5ffb1d6c88
Ashparshp <ashparsh.connects@gmail.com>
2025-04-01 12:08:00 +0530

    chi router

--- WebGo/02_Serving_HTML/cmd/web/routes.go
+++ WebGo/02_Serving_HTML/cmd/web/routes.go
@@ -5,14 +5,21 @@ import (
 	"templates/pkg/config"
 	"templates/pkg/handlers"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 )
 
 func routes(app *config.AppConfig) http.Handler {
+	
+	/*
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.HomePage))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.AboutPage))
+	*/
+
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.HomePage)
+	mux.Get("/about", handlers.Repo.AboutPage)
 
 	return mux
 }
