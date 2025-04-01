f426767aaf57316af82423b3224a67fb8bf37d8b
Ashparshp <ashparsh.connects@gmail.com>
2025-04-01 12:33:25 +0530

    csfr

--- WebGo/02_Serving_HTML/cmd/web/routes.go
+++ WebGo/02_Serving_HTML/cmd/web/routes.go
@@ -6,9 +6,10 @@ import (
 	"templates/pkg/handlers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func routes(app *config.AppConfig) http.Handler {
+func routes(_ *config.AppConfig) http.Handler {
 	
 	/*
 	mux := pat.New()
@@ -18,6 +19,10 @@ func routes(app *config.AppConfig) http.Handler {
 	*/
 
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
 	mux.Get("/", handlers.Repo.HomePage)
 	mux.Get("/about", handlers.Repo.AboutPage)
 
--- WebGo/02_Serving_HTML/go.mod
+++ WebGo/02_Serving_HTML/go.mod
@@ -3,3 +3,5 @@ module templates
 go 1.23.2
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require github.com/justinas/nosurf v1.1.1
