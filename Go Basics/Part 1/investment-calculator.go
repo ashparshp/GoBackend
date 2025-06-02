eef4348f800e48834033bf8d2ca32fe492e1d945
Ashparshp <ashparsh.connects@gmail.com>
2025-06-02 11:21:11 +0530

    Refactor expected return rate declaration to use shorthand notation

--- Go Basics/Part 1/investment-calculator.go
+++ Go Basics/Part 1/investment-calculator.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var investmentAmount float64 = 1000
-	var expectedReturnRate float64 = 5.5
+	expectedReturnRate := 5.5
 	var years float64 = 10
 
 	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
