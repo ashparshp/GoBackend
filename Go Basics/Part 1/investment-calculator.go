1defa39f01ce373b87b291ac5773debb5a48a0f2
Ashparshp <ashparsh.connects@gmail.com>
2025-06-03 10:33:49 +0530

    Refactor investment calculator to prompt for expected return rate input

--- Go Basics/Part 1/investment-calculator.go
+++ Go Basics/Part 1/investment-calculator.go
@@ -8,13 +8,18 @@ import (
 func investmentCalculator() {
 	const inflationRate = 2.5
 	var investmentAmount float64
-	expectedReturnRate := 5.5
+	var expectedReturnRate float64
 	var years float64 = 10
 
 	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
