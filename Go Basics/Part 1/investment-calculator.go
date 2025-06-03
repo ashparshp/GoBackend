c50f8b568d746a6743fce824659d62a85f2a3fa3
Ashparshp <ashparsh.connects@gmail.com>
2025-06-03 10:15:14 +0530

    investment calculator to prompt for user input and calculate future values

--- Go Basics/Part 1/investment-calculator.go
+++ Go Basics/Part 1/investment-calculator.go
@@ -6,10 +6,19 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
+	const inflationRate = 2.5
+	var investmentAmount float64
 	expectedReturnRate := 5.5
 	var years float64 = 10
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	fmt.Println(futureValue)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+
+	fmt.Printf("Future Value: %.2f\n", futureValue)
+	fmt.Printf("Future Real Value (adjusted for inflation): %.2f\n", futureRealValue)
 }
