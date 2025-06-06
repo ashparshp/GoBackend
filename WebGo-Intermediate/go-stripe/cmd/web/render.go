6b6fa0080bc29616c18d2994e94512dc083c8684
Ashparshp <ashparsh.connects@gmail.com>
2025-06-06 13:57:30 +0530

    Refactor templateData struct for clarity and add base layout template

--- WebGo-Intermediate/go-stripe/cmd/web/render.go
+++ WebGo-Intermediate/go-stripe/cmd/web/render.go
@@ -1,15 +1,24 @@
 package main
 
+import (
+	"embed"
+	"html/template"
+)
+
 type templateData struct {
-	StringMap map[string]string
-	IntMap map[string]int
-	FloatMap map[string]float32
-	Data map[string]interface{}
-	CSRFToken string
-	Flash string
-	Warning string
-	Error string
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
 	IsAuthenticated int
-	API string
-	CSSVersion string
+	API             string
+	CSSVersion      string
 }
+
+var functions = template.FuncMap {}
+
+var templateFS embed.FS
