b35ccf89c4ae8c9971e11d3c1e45f3bcc7bf953b
Ashparshp <ashparsh.connects@gmail.com>
2025-06-20 23:04:57 +0530

    Refactor payment processing: add Card struct and CreatePaymentIntent method; update dependencies in go.mod and go.sum

--- WebGo-Intermediate/go-stripe/go.mod
+++ WebGo-Intermediate/go-stripe/go.mod
@@ -2,4 +2,9 @@ module github.com/ashparshp/go-stripe
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.2.1
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/stripe/stripe-go v70.15.0+incompatible
+)
+
+require github.com/stripe/stripe-go/v72 v72.122.0
---
+++ WebGo-Intermediate/go-stripe/internal/card/card.go
@@ -0,0 +1,40 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, error, string) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	pi, err := paymentintent.New(params)
+	msg := ""
+	if err != nil {
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = string(stripeErr.Code)
+		}
+	}
+
+	return pi, err, msg
+}
