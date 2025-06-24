deab453788d4eebfee836ffb26a60d29f0e6d6f8
Ashparshp <ashparsh.connects@gmail.com>
2025-06-24 09:23:44 +0530

    Add database connection configuration and update serve function to handle DB connection

--- WebGo-Intermediate/go-stripe/cmd/web/main.go
+++ WebGo-Intermediate/go-stripe/cmd/web/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/ashparshp/go-stripe/internal/driver"
 )
 
 const version = "1.0.0"
@@ -15,33 +17,33 @@ const cssVersion = "1"
 
 type config struct {
 	port int
-	env string
-	api string
-	db struct {
+	env  string
+	api  string
+	db   struct {
 		dsn string
 	}
 	stripe struct {
 		secret string
-		key string
+		key    string
 	}
 }
 
 type application struct {
-	config config
-	infoLog *log.Logger
-	errorLog *log.Logger
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
 	templateCache map[string]*template.Template
-	version string
+	version       string
 }
 
 func (app *application) serve() error {
-	srv := &http.Server {
-		Addr: fmt.Sprintf(":%d", app.config.port),
-		Handler: app.routes(),
-		IdleTimeout: 30 * time.Second,
-		ReadTimeout: 10 * time.Second,
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
 		ReadHeaderTimeout: 5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
 	}
 
 	app.infoLog.Printf("Starting HTTP server in %s mode on port %d", app.config.env, app.config.port)
@@ -52,6 +54,7 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.db.dsn, "dsb", "ashparsh:secret@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
 
 	flag.Parse()
@@ -62,17 +65,23 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
+
 	tc := make(map[string]*template.Template)
 
-	app := &application {
-		config: cfg,
-		infoLog: infoLog,
-		errorLog: errorLog,
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
 		templateCache: tc,
-		version: version,
+		version:       version,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		app.errorLog.Println(err)
 		log.Fatal(err)
