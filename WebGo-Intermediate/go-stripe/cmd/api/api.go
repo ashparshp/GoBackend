f350d07932f5af314cda9a622f08c5061cf1256b
Ashparshp <ashparsh.connects@gmail.com>
2025-06-25 10:36:34 +0530

    Add GetWidgetByID handler and route for retrieving widget details by ID

--- WebGo-Intermediate/go-stripe/cmd/api/api.go
+++ WebGo-Intermediate/go-stripe/cmd/api/api.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ashparshp/go-stripe/internal/driver"
+	"github.com/ashparshp/go-stripe/internal/models"
 )
 
 const version = "1.0.0"
@@ -30,6 +31,7 @@ type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
 	version  string
+	DB       models.DBModel
 }
 
 func (app *application) serve() error {
@@ -73,6 +75,7 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		version:  version,
+		DB:       models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
