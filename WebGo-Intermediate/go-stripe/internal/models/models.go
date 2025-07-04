b02cac781f67fa319135ce2a97b4484dafcb97c2
Ashparshp <ashparsh.connects@gmail.com>
2025-07-04 10:07:04 +0530

    Add payment_intent and payment_method fields to Transaction model

--- WebGo-Intermediate/go-stripe/internal/models/models.go
+++ WebGo-Intermediate/go-stripe/internal/models/models.go
@@ -73,6 +73,8 @@ type Transaction struct {
 	ExpiryMonth         int       `json:"expiry_month"`
 	ExpiryYear          int       `json:"expiry_year"`
 	BankReturnCode      string    `json:"bank_return_code"`
+	PaymentIntent       string    `json:"payment_intent"`
+	PaymentMethod       string    `json:"payment_method"`
 	TransactionStatusID int       `json:"transaction_status_id"`
 	CreatedAt           time.Time `json:"-"`
 	UpdatedAt           time.Time `json:"-"`
